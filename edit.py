9abd768db83ae9fd876f3acff221030f90a8d030
ghazi <ghazi-z3balawi@hotmail.com>
2020-11-18 03:03:44 -0800

    all files are updated, more yet to come

--- edit.py
+++ edit.py
@@ -11,22 +11,26 @@ class Edit:
         self.edit = Menu(menubar.toolbar, tearoff=False)
 
     def edit_menu(self):
-        self.edit.add_command(label="Copy", command=lambda: self.copy(),accelerator='Ctrl+C')
         self.edit.add_command(
-            label="Paste", command=lambda: self.paste(),accelerator='Ctrl+V'
+            label="Copy", command=lambda: self.copy(), accelerator="Ctrl+C"
         )
         self.edit.add_command(
-            label="Cut", command=lambda: self.cut(),accelerator='Ctrl+X'
+            label="Paste", command=lambda: self.paste(), accelerator="Ctrl+V"
         )
         self.edit.add_command(
-            label="Redo", command=lambda: self.redo(),accelerator='Ctrl+Y'
-        )
-        self.edit.add_command(label="Undo", command=lambda: self.undo(),accelerator='Ctrl+Z')
-        self.edit.add_command(
-            label="Select all", command=lambda: self.select_all(),accelerator='Ctrl+A'
+            label="Cut", command=lambda: self.cut(), accelerator="Ctrl+X"
         )
         self.edit.add_command(
-            label="Delete", command=lambda: keyboard.press("DEL"),accelerator='DEL'
+            label="Redo", command=lambda: self.redo(), accelerator="Ctrl+Y"
+        )
+        self.edit.add_command(
+            label="Undo", command=lambda: self.undo(), accelerator="Ctrl+Z"
+        )
+        self.edit.add_command(
+            label="Select all", command=lambda: self.select_all(), accelerator="Ctrl+A"
+        )
+        self.edit.add_command(
+            label="Delete", command=lambda: keyboard.press("DEL"), accelerator="DEL"
         )
         menubar.toolbar.add_cascade(label="Edit", menu=self.edit)
         w.root.config(menu=self.edit)
