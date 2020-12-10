1dcfb5293a5f5be52201ef336da8428990da41af
ghazi <ghazi-z3balawi@hotmail.com>
2020-12-10 17:03:41 +1300

    changed the default font size to 15, changed default text backgroud, and changed the default font family to Consolas

--- textconfig.py
+++ textconfig.py
@@ -1,12 +1,15 @@
+"""Made  a class for text configuration, so it is possible and easier to 
+control text configurations around all widgets at once,also can be used to edit some other widgets background"""
+
+
 class TextConfig:
     def __init__(self):
         self.insertbackground = "white"
-        self.background = "gray20"
+        self.background = "#282828"
         self.foreground = "white"
         self.selectbackground = "blue"
-        self.font_family = "Arial"
-        self.font_size = 16
+        self.font_family = "Consolas"
+        self.font_size = 15
 
 
 txtconfig = TextConfig()
-"""Made  a class for text configuration, so it is possible and easier to control text configurations around all widgets at once"""
