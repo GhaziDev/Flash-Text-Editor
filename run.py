8720703b636a252b5a8c510ee24ef77571824ce7
ghazi <ghazi-z3balawi@hotmail.com>
2021-01-08 20:04:53 +1300

    file updated to include tools object

--- run.py
+++ run.py
@@ -6,16 +6,18 @@ from view import View
 from search import Search
 from theme import Themes
 from interpret_and_compile import Run
-
+from tools import Tools
 t = Themes()
 r = Run()
 e = Edit()
 v = View()
 s = Search()
+tl = Tools()
 e.edit_menu()
 v.view_menu()
 s.search_menu()
 t.theme_menu()
+tl.tools_menu()
 t.dark_theme()
 w.root.config(menu=rcm.rightclickmenu)
 w.root.config(menu=menubar.toolbar)
